cd7ca67c3f9cdbf6d7dc465e39468cef1a27ddf6
Jacob Tran <jacobtran@gmail.com>
2016-07-28 14:05:33 -0700

    day 9

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// alert("Inside main.js");
+
+// go somewhere else??
+// google?
+document.write("Hello?");
+// document.getElementsByTagName("h1")[0].innerHTML = "Contact ABC Daycare";
+// document.getElementById("contact-us").innerHTML = "Contact ID Daycare";
+var obj = document.getElementById("contact-us");
+obj.innerHTML = "Contact OBJ Daycare";
+obj.style.backgroundColor = "gold";
